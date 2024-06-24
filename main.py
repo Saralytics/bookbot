050f1c9cf2abe17bc82e870759a952ef6dc809af
Saralytics <zhaoxue.li@outlook.com>
2024-06-24 20:25:42 +0400

    prepare to dockerize

--- main.py
+++ main.py
@@ -1,58 +1,53 @@
-def main(path_to_file: str) -> str:
-    with open(path_to_file) as f:
-        contents = f.read()
-        return contents
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-def wordcount(contents: str):
-    words = contents.split()
-    return len(words)
-
-
-def count_letters(contents: str) -> list:
-    # Generate a list of lowercase ASCII characters
-    ascii_lowercase = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-
-    # Initialize a dictionary with lowercase ASCII characters as keys
-    ascii_dict = {char: 0 for char in ascii_lowercase}
-
-    for i in contents:
-        char = i.lower()
-        if char in ascii_dict.keys():
-            ascii_dict[char] += 1
-
-    chars = []
-    for key, value in ascii_dict.items():
-        new_dict = {"char": key, "val": value}
-        chars.append(new_dict)
-
-    # print(chars)
-
-    def sort_on(ascii_dict):
-        return ascii_dict['val']
-
-    chars.sort(reverse=True, key=sort_on)
-
-    return chars
-
-    # get a list of lower ascii letters
-    # initiate a dictionary
-    # update the dictionary while traversing over the contents
-
-
-def report(contents: str) -> str:
-    wc = wordcount(contents)
-    chars_list = count_letters(contents)
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{wc} words found in the document")
-    print("")
-
-    for i in chars_list:
-        print(f"The {i['char']} character was found {i['val']} times")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
     print("--- End report ---")
 
 
-if __name__ == "__main__":
-    contents = main('books/frankenstein.txt')
-    report(contents)
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
